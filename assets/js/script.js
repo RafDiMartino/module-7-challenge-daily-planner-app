40ffc6a1c56474cea59969839fa1a03d9bdab3f3
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-20 18:05:31 +0000

    adding current time at the top of the calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("hey")
+const currentDay = $("#currentDay");
+
+// handle displaying the time
+function displayTime() {
+    let today = moment().format('dddd, MMMM Do');
+    currentDay.text(today);
+  }
+
+displayTime()
