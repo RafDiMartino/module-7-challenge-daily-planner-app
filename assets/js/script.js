ea7822e288d08631d1054b5af3dd73e22ccfac4e
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-01-21 17:40:20 +0000

    working on styling the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,22 @@ for (let i = hours.start; i <= hours.end; i++) {
   
   // creating the elent and appending them to the div container
   let textAreaWrapper = $("<div>")
+  textAreaWrapper.addClass("wrapper")
   container.append(textAreaWrapper)
   
   let btnSave = $("<button>")
   btnSave.addClass("saveBtn")
   btnSave.attr("data-hour", i)
-  btnSave.html("<i class='fa-solid fa-floppy-disk'></i>")
+  btnSave.html("<i class='fas fa-save'></i>")
   // Event listener that fires the saveBtnHandler
   btnSave.on("click", saveBtnHandler)
-  let label = $("<label>")
+  let label = $("<span>")
   let textarea = $("<textarea>")
   textarea.attr("id", i)
   textarea.val(localStorage.getItem(i))
   
   let displayHour = moment(`2023-01-01T${i < 10 ? "0" + i : i}:00:00`).format("ha")
-  textAreaWrapper.append(label).text(displayHour)
+  textAreaWrapper.append(label).html(`<span class="hour">${displayHour}</span>`)
   textAreaWrapper.append(textarea)
   textAreaWrapper.append(btnSave)
   let currentTime = moment().format("H")
@@ -61,10 +62,12 @@ function saveBtnHandler(e) {
 
 // Function that handle the saved message display
 function displaySavedMessage() {
-  let displayMessage = $("#saved-message")
+  
+  const displayMessage = $("#saved-message")
   displayMessage.toggleClass("hideMessage");
   setTimeout(() => {
   displayMessage.toggleClass("hideMessage")
+
   }, 2000)
 
 }
